efc620cc27f6a24b5e98a7c4582716db47ea461b
Sanbei <sanbei101@outlook.com>
2024-09-19 06:18:05 +0000

    一点小小的更改

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,37 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import viteCompression from 'vite-plugin-compression';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     vue(),
+
     viteCompression({
       verbose: true,
       disable: false,
       threshold: 10240,
-      algorithm: "gzip",
-      ext: ".gz",
-    }),
+      algorithm: 'gzip',
+      ext: '.gz'
+    })
   ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
   build: {
-    target: "esnext",
+    target: 'esnext',
     rollupOptions: {
       output: {
         manualChunks: {
-          "vue-bundle": ["vue"],
+          'vue-bundle': ['vue']
         },
-        entryFileNames: "assets/[name].[hash].js",
-        chunkFileNames: "assets/[name].[hash].js",
-        assetFileNames: "assets/[name].[hash].[ext]",
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
       },
-      external: ["fsevents"],
-    },
-  },
-});
+      external: ['fsevents']
+    }
+  }
+})
